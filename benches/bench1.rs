8c85e1b45c1ee0efa8ca96eef0711c049e24a742
William Blake <you@example.com>
2023-06-02 11:09:07 +0300

    add bench for v4

--- benches/bench1.rs
+++ benches/bench1.rs
@@ -14,6 +14,9 @@ fn criterion_benchmark(c: &mut Criterion) {
     g.bench_function(BenchmarkId::new("v3", "3"), |b| {
         b.iter(|| test_palindroms::pal_v3(black_box(test_string)))
     });
+    g.bench_function(BenchmarkId::new("v4", "4"), |b| {
+        b.iter(|| test_palindroms::pal_v4(black_box(test_string)))
+    });
     g.finish();
 }
 criterion_group!(benches, criterion_benchmark);
