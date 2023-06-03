8331cc86879dfc7b4c6edd8d698ce75a02b44950
William Blake <you@example.com>
2023-06-03 12:56:54 +0300

    misc changes in benchmarks naming

--- benches/bench1.rs
+++ benches/bench1.rs
@@ -5,16 +5,16 @@ fn criterion_benchmark(c: &mut Criterion) {
     let test_string = "a ,roza upa__la na lapu azora!a ,roza upa__la na lapu azora!a ,roza upa__la na lapu azora!a ,roza upa__la na lapu azora!";
     let mut g = c.benchmark_group("Palindrom");
 
-    g.bench_function(BenchmarkId::new("v1", "1"), |b| {
+    g.bench_function(BenchmarkId::new("v", "1"), |b| {
         b.iter(|| test_palindroms::pal_v1(black_box(test_string)))
     });
-    g.bench_function(BenchmarkId::new("v2", "2"), |b| {
+    g.bench_function(BenchmarkId::new("v", "2"), |b| {
         b.iter(|| test_palindroms::pal_v2(black_box(test_string)))
     });
-    g.bench_function(BenchmarkId::new("v3", "3"), |b| {
+    g.bench_function(BenchmarkId::new("v", "3"), |b| {
         b.iter(|| test_palindroms::pal_v3(black_box(test_string)))
     });
-    g.bench_function(BenchmarkId::new("v4", "4"), |b| {
+    g.bench_function(BenchmarkId::new("v", "4"), |b| {
         b.iter(|| test_palindroms::pal_v4(black_box(test_string)))
     });
     g.finish();
