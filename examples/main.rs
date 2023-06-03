dd6d54a987d940ac80304a70018d5e676544ea82
William Blake <you@example.com>
2023-06-03 13:53:44 +0300

    add helpers, ready to go

--- examples/main.rs
+++ examples/main.rs
@@ -1,21 +1,24 @@
 use test_palindroms;
 
-fn run(s: &str, f: Box<dyn Fn(&str) -> bool>) -> bool {
+fn run(s: &str, f: &dyn Fn(&str) -> bool) -> bool {
     f(s)
 }
 
-fn present(b: bool) -> &'static str {
-    match b {
-        true => "",
-        false => "not",
-    }
+fn present(s: &str, b: bool) -> String {
+    format!(
+        "\"{0}\" is {1} palindrom.",
+        s,
+        match b {
+            true => "",
+            false => "not",
+        }
+    )
 }
 
 fn main() {
-    let s = "hello, world!";
+    let s = "hi,ih!";
     println!(
-        "so far so good: \"{0}\" is {1} palindrom!",
-        s,
-        present(run(s, Box::new(test_palindroms::pal_v1)))
+        "so far so good.\n{}",
+        present(s, run(s, &test_palindroms::pal_v1))
     );
 }
