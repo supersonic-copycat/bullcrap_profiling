cbaa3aa51271cf3de70faf2fd70f9eaa5facd9e6
Anatolii <a@gryzlov.ru>
2023-06-03 18:07:30 +0300

    simplify example program, enable debug symbols in rel build

--- examples/main.rs
+++ examples/main.rs
@@ -1,9 +1,5 @@
 use test_palindroms;
 
-fn run(s: &str, f: &dyn Fn(&str) -> bool) -> bool {
-    f(s)
-}
-
 fn present(s: &str, b: bool) -> String {
     format!(
         "\"{0}\" is {1} palindrom.",
@@ -18,8 +14,13 @@ fn present(s: &str, b: bool) -> String {
 fn main() {
     let s = "a ,roza upa__la na lapu azora!a ,roza upa__la na lapu azora!a ,roza upa__la na lapu azora!a ,roza upa__la na lapu azora!";
     let cap = 10_000;
-    let mut r = Vec::with_capacity(cap);
+    let mut r = Vec::with_capacity(cap * 2);
     for _ in 0..cap {
-        r.push(std::hint::black_box(run(s, &test_palindroms::pal_v1)));
+        r.push(std::hint::black_box(test_palindroms::pal_v1(s)));
     }
+    println!("ended1");
+    for _ in 0..cap {
+        r.push(std::hint::black_box(test_palindroms::pal_v4(s)));
+    }
+    println!("ended2");
 }
